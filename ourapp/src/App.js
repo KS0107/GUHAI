c6de55133dfcb8cd4903c70ecd2e0af6dacf301e
Bailey-GH <71499203+Bailey-GH@users.noreply.github.com>
2021-11-28 12:16:22 +0000

    added cards

--- ourapp/src/App.js
+++ ourapp/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Card2 from './Card2';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Testing.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Card2
+        title='Who are we?'
+        image='./images/questionmark.jpg'
+        body='We are your last hope. In this day and age you need someone to guide you. We are that someone. Let us aid you on where to go to ensure your safety.'
+        />
+        <Card2
+        title='How we survived?'
+        image='./images/wasteland.jpg'
+        body="We are only here because of our big brains. In a world like this, you're gonna need big brains."
+        />
+      </div>
+    </>
   );
 }
 
